16bccc15d55f9e613cc8d0eeb7a5019f1b21d2d2
Nicodemus <nicodemusdemuse@gmail.com>
2019-11-14 16:00:08 +0700

    add provider & validator

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:login_bloc/blocs/bloc.dart';
+import 'package:login_bloc/blocs/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(bloc),
+          passwordField(bloc),
+          SizedBox(height: 25),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot){
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'you@example.com',
+            labelText: 'Email Address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot){
+        return TextField(
+          onChanged: bloc.changePassword,
+//        obscureText: true,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      textColor: Colors.white,
+      onPressed: () {},
+    );
+  }
+}
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:login_bloc/screens/LoginScreen.dart';
+import 'package:login_bloc/screens/login_screen.dart';
+import 'package:login_bloc/blocs/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
       ),
     );
   }
